31736fb597520a097be960a1a5632f558e4814fc
Jérôme Laban <jerome@platform.uno>
2022-12-09 11:38:27 -0500

    Version update (#3)
    * fix: Adjust Readme
    
    * chore: Upgrade dependencies
    
    * fix: Download assets through download URL
    
    * fix: Adjust versioning
    
    * fix: Support for linux
    
    * fix: Adjust tags logging message
    
    * ci: version bump
    
    +semver:minor

--- src/dotnet-azdo-artifacts-multidownload/AzureDevopsDownloader.cs
+++ src/dotnet-azdo-artifacts-multidownload/AzureDevopsDownloader.cs
@@ -6,12 +6,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace dotnet_azdo_artifacts_multidownload
 {
 	class AzureDevopsDownloader
 	{
+		private readonly string DirectorySeparator = Path.DirectorySeparatorChar.ToString();
+
 		private readonly string _pat;
 		private readonly string _collectionUri;
 
@@ -41,7 +44,10 @@ namespace dotnet_azdo_artifacts_multidownload
 				sourceBranchName = "refs/heads/" + sourceBranchName;
 			}
 
-			Console.WriteLine($"Getting definitions ({basePath}, {project}, {definitionName}, {artifactName}, {sourceBranchName}, {buildId}, {downloadLimit})");
+			Console.WriteLine($"Getting definitions (" +
+				$"basePath:{basePath}, project:{project}, definition:{definitionName}, artifact:{artifactName}," +
+				$" branch:{sourceBranchName}, buildId:{buildId}, limit:{downloadLimit}, tags:{string.Join(",", tags)})");
+
 			var definitions = await client.GetDefinitionsAsync(project, name: definitionName);
 
 			Console.WriteLine("Getting builds");
@@ -63,7 +69,7 @@ namespace dotnet_azdo_artifacts_multidownload
 				.Concat(new[] { currentBuild });
 
 			string BuildArtifactPath(Build build)
-				=> Path.Combine(basePath, $@"artifacts\{build.LastChangedDate:yyyyMMdd-hhmmss}-{build.Id}");
+				=> Path.Combine(basePath, "artifacts", $@"{build.LastChangedDate:yyyyMMdd-hhmmss}-{build.Id}");
 
 			foreach (var build in suceededBuilds)
 			{
@@ -78,7 +84,12 @@ namespace dotnet_azdo_artifacts_multidownload
 					if (artifacts.Any(a => a.Name == artifactName))
 					{
 						Console.WriteLine($"Getting artifact for build {build.Id}");
-						using (var stream = await client.GetArtifactContentZipAsync(project, build.Id, artifactName))
+
+						var artifact = await client.GetArtifactAsync(project, build.Id, artifactName);
+
+						using var downloadClient = new HttpClient();
+
+						using (var stream = await downloadClient.GetStreamAsync(artifact.Resource.DownloadUrl))
 						{
 							using (var f = File.OpenWrite(tempFile))
 							{
@@ -88,23 +99,32 @@ namespace dotnet_azdo_artifacts_multidownload
 
 						Console.WriteLine($"Extracting artifact for build {build.Id}");
 
-						fullPath = fullPath.Replace("\\\\", "\\");
+						fullPath = fullPath.Replace(
+							DirectorySeparator + DirectorySeparator,
+							DirectorySeparator);
 
 						using (var archive = ZipFile.OpenRead(tempFile))
 						{
 							foreach (var entry in archive.Entries)
 							{
-								var outPath = Path.Combine(fullPath, entry.FullName.Replace("/", "\\"));
+								var outPath = Path.Combine(fullPath, entry.FullName.Replace("/", DirectorySeparator));
 
-								if (outPath.EndsWith("\\"))
+								if (outPath.EndsWith(Path.DirectorySeparatorChar))
 								{
-									Directory.CreateDirectory(@"\\?\" + outPath);
+									Directory.CreateDirectory(outPath);
 								}
 								else
 								{
+									var directoryName = Path.GetDirectoryName(outPath)!;
+
+									if (!Directory.Exists(directoryName))
+									{
+										Directory.CreateDirectory(directoryName);
+									}
+
 									using (var stream = entry.Open())
 									{
-										using (var outStream = File.OpenWrite(@"\\?\" + outPath))
+										using (var outStream = File.OpenWrite(outPath))
 										{
 											await stream.CopyToAsync(outStream);
 										}
